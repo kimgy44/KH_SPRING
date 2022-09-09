fd7ab0fc040bafc86217832ff1710da757ee95b7
kimgy44 <kimgy44@naver.com>
2022-09-09 23:43:23 +0900

    0906 수업

--- basic/src/main/java/com/basic/step1/AuthDao.java
+++ basic/src/main/java/com/basic/step1/AuthDao.java
@@ -3,13 +3,14 @@ package com.basic.step1;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.apache.log4j.Logger;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.vo.MemberVO;
-// DAO뒤에서는 오라클 서버와 연동하기
-// myBatis 레이어에 대한 설정이 필요하다
+// DAO뒤에서는 오라클서버와 연동하기
+// myBatis 레이어 대한 설정이 필요하다.
 @Service
 public class AuthDao {
 	// SqlSessionTemplate는 mybatis-spring.jar에서 제공하는 클래스로
@@ -21,16 +22,14 @@ public class AuthDao {
 		//logger.info("login 호출 성공");
 		MemberVO memVO = null;
 		Map<String,Object> pMap = new HashMap<>();
-		pMap.put("mem_id", "test");
+		pMap.put("mem_id", "tomato");
 		pMap.put("mem_pw", "123");
-		// selectOne(One은 Object)은 조회건 수가 반드시 한건이어야 한다.
-		// Too Many . . . 
+		// selectOne(One은 Object-오직 1건)은 조회건 수가 반드시 한건이어야 한다.
+		// Too Many....
 		memVO = sqlSessionTemplate.selectOne("login", pMap);
-	    if(memVO!=null) {
-	    //  logger.info(memVO.getMem_name());
-	    }
-	    //sqlSessionTemplate.selectOne("login", pMap);
-	    // //logger.info(memVO);
-	     return memVO;
+		if(memVO!=null) {
+		//logger.info(memVO.getMem_name());			
+		}
+		return memVO;
 	}
 }
